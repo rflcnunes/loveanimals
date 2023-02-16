d80b7ed3a49abd5b49407960af5ff56f88bce98d
rafael <rafaelcnunes@icloud.com>
2023-02-16 01:46:31 -0300

    added scss variables globally

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "@/styles/_variables.scss";
+        `,
+      },
+    },
+  },
+};
